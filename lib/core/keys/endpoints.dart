2eb9ee15a5a9171fd7403a6294007ef200bae1e0
Akintade Oluwaseun <akintadeseun816@gmail.com>
2025-01-23 03:02:17 +0100

    set up api auth services

---
+++ lib/core/keys/endpoints.dart
@@ -0,0 +1,29 @@
+class Endpoints {
+  Endpoints._();
+
+  // receiveTimeout
+  static const int receiveTimeout = 15000;
+
+  // connectTimeout
+  static const int connectionTimeout = 15000;
+}
+
+class _Auth {
+  final login = "/api/v1/auth/login";
+  final signup = "/api/v1/auth/signup";
+  final verifyOtp = "/api/v1/auth/verify";
+  final forgotPassword = "/api/v1/auth/forgotpassword";
+  final resendVerificationCode = "/api/v1/auth/resendverification";
+  final resetPassword = "/api/v1/auth/resetpassword";
+  final updatePassword = "/api/v1/auth/updatepassword";
+}
+
+class _Profile {
+  final getProfile = "/api/v1/profile/get-profile";
+  final setProfile = "/api/v1/profile/set-profile";
+  final uploadImage = "/api/v1/profile/upload-image";
+  final deleteAccount = "/api/v1/profile/delete-profile";
+}
+
+final auth = _Auth();
+final userProfile = _Profile();
